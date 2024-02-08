3bce2015a1539d4c1fd290207565b19ed98be35b
andie7 <131781648+andie7@users.noreply.github.com>
2024-02-08 18:01:30 +0100

    weather app set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="Container">
+        <Weather city="New York" />
+
+        <footer className="footer">
+          This project was coded by Andreia Fernandes and is
+          <a
+            href="https://github.com/andie7/New-React-weather-app"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import WeatherInfo from "./WeatherInfo";
+
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState(props.city);
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      date: new Date(response.data.dt * 1000),
+    });
+    setReady(true);
+  }
+
+  function search() {
+    let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading";
+  }
+}
