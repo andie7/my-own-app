b7df202e969510f3e17b2e38bce4915fdb6ba7d1
andie7 <131781648+andie7@users.noreply.github.com>
2024-02-08 18:11:39 +0100

    Fortmatting

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -28,8 +28,8 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity:{props.data.humidity}%</li>
-            <li>Wind:{props.data.wind} Km/h</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} Km/h</li>
           </ul>
         </div>
       </div>
