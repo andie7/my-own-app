40c3ddab2791c8a2b8c3ae421c9d70056c5349b2
andie7 <131781648+andie7@users.noreply.github.com>
2024-02-10 11:52:27 +0100

    footer changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ export default function App() {
         <Weather city="New York" />
 
         <footer className="footer">
-          This project was coded by Andreia Fernandes and is
+          This project was coded by {""}
+          <a
+            href="https://linktr.ee/andiekay90"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Andreia Fernandes
+          </a>{" "}
+          {""}
+          and is {""}
           <a
             href="https://github.com/andie7/my-own-app"
             target="_blank"
@@ -17,7 +26,7 @@ export default function App() {
           >
             open sourced on Github
           </a>
-          and
+          {""} and {""}
           <a href="" target="_blank" rel="noreferrer">
             hosted on Netlify
           </a>
